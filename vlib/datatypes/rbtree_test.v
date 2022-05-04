00cf0c5fa0dd80d50a0dddbec3f33fb15ef322a9
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-05-04 13:34:20 +0200

    datatypes: add test on insert
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- vlib/datatypes/rbtree_test.v
+++ vlib/datatypes/rbtree_test.v
@@ -4,3 +4,13 @@ fn test_init_rbtree() {
 	rbtree := RBTree<int>{}
 	assert rbtree.is_empty()
 }
+
+fn test_insert_into_rbtree() {
+	mut rbtree := RBTree<int>{}
+	assert rbtree.insert(12)
+	assert rbtree.insert(23)
+	assert rbtree.insert(45)
+
+	assert rbtree.contains(12)
+	assert rbtree.contains(34) == false
+}
