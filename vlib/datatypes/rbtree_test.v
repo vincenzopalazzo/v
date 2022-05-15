8e6fea3b6d5f02f04b87e698cbb04c9022189bb8
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-05-15 15:12:36 +0200

    datatype: add test for the delete operation
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- vlib/datatypes/rbtree_test.v
+++ vlib/datatypes/rbtree_test.v
@@ -14,3 +14,16 @@ fn test_insert_into_rbtree() {
 	assert rbtree.contains(12)
 	assert rbtree.contains(34) == false
 }
+
+fn test_remove_into_rbtree() {
+	mut rbtree := RBTree<int>{}
+	assert rbtree.insert(12)
+	assert rbtree.insert(23)
+	assert rbtree.insert(45)
+	assert rbtree.insert(34)
+	assert rbtree.contains(12)
+	assert rbtree.contains(34)
+
+	assert rbtree.remove(34)
+	assert rbtree.contains(34) == false
+}
